e7548491137e1580124b90234e58904764828242
Piet Bohn <piet.bohn@web.de>
2023-11-14 11:38:28 +0100

    refactoring

--- src/controllers/workflows.controller.ts
+++ src/controllers/workflows.controller.ts
@@ -1,15 +1,13 @@
-import { Body, Post, Route, Tags } from 'tsoa';
+import { Get, Route, Tags } from 'tsoa';
 import {GetReposAndWorkflows} from "../GHAFilesAndCharacteristics/GetReposAndWorkflows";
 
 @Route("workflows")
 @Tags("Workflows")
 export default class WorkflowsController {
 
-    @Post("/")
+    @Get("/")
     public async getWorkflows(): Promise<any> {
-        console.log("ja");
         let res: any = await new GetReposAndWorkflows().get();
-        console.log(res);
         return res;
     }
 }
--- src/routes/workflows.router.ts
+++ src/routes/workflows.router.ts
@@ -3,7 +3,7 @@ import WorkflowsController from "../controllers/workflows.controller";
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.get("/", async (req, res) => {
     const controller = new WorkflowsController();
     const response = await controller.getWorkflows();
     return res.send(response);
