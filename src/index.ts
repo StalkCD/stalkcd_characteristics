a94642c26c414135f9c8795460c97a6912ce1d7f
Piet Bohn <piet.bohn@web.de>
2023-11-29 12:48:50 +0100

    cors changes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import { Application } from "express";
-import express = require("express");
 import morgan = require("morgan");
 import Router from "./routes";
 import swaggerUi = require("swagger-ui-express");
+
+const express = require("express");
 const cors = require("cors");
+const app =express();
 
-const PORT = process.env.PORT || 8082;
+const corsOptions = {
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // Erlaube Cookies und andere Anmeldeinformationen
+  optionsSuccessStatus: 204, // Einige Legacy-Browser (IE11) erfordern 204
+};
 
-const app: Application = express();
-
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(morgan("tiny"));
 app.use(express.static("public"));
@@ -26,6 +30,7 @@ app.use(
 
 app.use(Router);
 
+const PORT = process.env.PORT || 8082;
 app.listen(PORT, () => {
   console.log("Server is running on port", PORT);
 });
